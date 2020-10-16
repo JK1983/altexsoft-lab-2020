786ebf7bea8de891978edc944191bcd878a1b434
JUK <ulya.krasnopolskaya@gmail.com>
2020-10-16 22:10:46 +0300

    Initial commit for SharedKernel

---
+++ CookBook/SharedKernel/SharedKernel/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedKernel
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
