7cda3851b38e2ad5a8ef4a815c50cceb9e54b636
JK1983 <ulya.krasnopolskaya@gmail.com>
2020-10-18 08:58:45 +0300

    web progect was added

---
+++ CookBook/UI/MVC/ASPNETCOREMVCUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCOREMVCUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
